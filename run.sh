82d2bbe99a79ecffd2f94f826356e18020a67c74
HaojunYuPKU <1700012804@pku.edu.cn>
2022-09-29 23:23:37 +0800

    support seen & unseen categories

--- run.sh
+++ run.sh
@@ -5,11 +5,10 @@ bash tools/dist_train.sh \
 configs/segmenter/segmenter-weak_vit-b16_160k_bs16_in21k_ade_all_no_attn_prior0.01.py
 
 # bash tools/dist_test.sh \
-# configs/segmenter/evaluation/baseline_no_attn_eval_ade_oracle.py \
+# configs/segmenter/evaluation/baseline_no_attn_eval_ade_oracle1.py \
 # work_dirs/202209227_baseline_no_attn_160k_bs16_coco/iter_160000.pth \
 # 4 \
-# --eval mIoU
-
+# --eval mIoU > clip1.txt
 
 # sudo nvidia-docker run --ipc=host -it -v /mnt/haojun/itpsea4data:/itpsea4data --ipc=host hsfzxjy/mmseg:pytorch1.8.1-cuda10.2-cudnn7-devel /bin/bash
 # pip install -U amlt --extra-index-url https://msrpypi.azurewebsites.net/stable/7e404de797f4e1eeca406c1739b00867 --extra-index-url https://azuremlsdktestpypi.azureedge.net/K8s-Compute/D58E86006C65
