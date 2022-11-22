080e82b95639f50281eea65fe47fd5638fa633ca
HaojunYuPKU <1700012804@pku.edu.cn>
2022-11-22 20:45:56 +0800

    remove some files

--- aml/install_aml_env.sh
+++
@@ -1,7 +0,0 @@
-pip install --upgrade azureml-sdk
-
-# itp lib
-pip install --upgrade --disable-pip-version-check --extra-index-url https://azuremlsdktestpypi.azureedge.net/ITP-Compute/D58E86006C65 azureml_contrib_itp
-
-# k8s lib
-pip install --upgrade --disable-pip-version-check --extra-index-url https://azuremlsdktestpypi.azureedge.net/K8s-Compute/D58E86006C65 azureml_contrib_k8s
--- install.sh
+++
@@ -1,7 +0,0 @@
-pip install mmcv-full==1.3.12 -f https://download.openmmlab.com/mmcv/dist/cu113/torch1.9.0a0+2ecb2c7/index.html
-pip install -e .
-cd third_party/CLIP
-pip install -e .
-cd ../../../detectron2
-pip install -e .
-cd ../mmseg
